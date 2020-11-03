557b9ba517076a83cad551dd5d984b38e4865ce4
Sam Quan <sammiequan@gmail.com>
2020-11-03 13:33:55 -0800

    Add basic navigation from main fragment screen to the category fragment screen.

--- app/src/main/java/com/balenciaga/fragments/CategoryFragment.kt
+++ app/src/main/java/com/balenciaga/fragments/CategoryFragment.kt
@@ -5,12 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.balenciaga.R
+import androidx.navigation.fragment.navArgs
+import com.balenciaga.databinding.FragmentCategoryBinding
 
 class CategoryFragment : Fragment() {
 
+    private var _binding : FragmentCategoryBinding? = null
+    private val binding
+        get() = _binding!!
+
+    private val args: CategoryFragmentArgs by navArgs()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category, container, false)
+        _binding = FragmentCategoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.categoryTextView.text = args.category.toUpperCase()
     }
 }
--- app/src/main/java/com/balenciaga/fragments/MainFragment.kt
+++ app/src/main/java/com/balenciaga/fragments/MainFragment.kt
@@ -5,12 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.balenciaga.R
+import androidx.navigation.fragment.findNavController
+import com.balenciaga.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
+    private var _binding : FragmentMainBinding? = null
+    // Property is only valid between onCreateView and onDestroyView
+    private val binding
+        get() = _binding!!
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        // return inflater.inflate(R.layout.fragment_main, container, false)
+
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.womenTextView.setOnClickListener {
+            navigateToCategoryFragment("women")
+        }
+        binding.allTextView.setOnClickListener {
+            navigateToCategoryFragment("all")
+        }
+        binding.menTextView.setOnClickListener {
+            navigateToCategoryFragment("men")
+        }
+    }
+
+    private fun navigateToCategoryFragment(category: String) {
+        val directions = MainFragmentDirections.navigateToCategoryFragment(category)
+        findNavController().navigate(directions)
     }
 }
